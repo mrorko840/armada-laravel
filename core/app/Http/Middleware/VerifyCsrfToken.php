27121e6a3b857b5ef54f56515f517a1f9c1f501c
Hemel Mridha <mrorko840@gmail.com>
2025-03-18 02:40:41 +0600

    dasdas

--- core/app/Http/Middleware/VerifyCsrfToken.php
+++ core/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
+        'admin/setting/update',
         '/paytm/notify',
         '/sslcommerz/notify',
         'razorpay/notify',
